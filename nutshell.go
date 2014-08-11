81e5d4a5d8cf7ec943681a3f8d2f397e0350b1a8
Ansgar <astrothe@umich.edu>
2014-08-11 12:54:56 -0400

    fix

--- nutshell.go
+++ nutshell.go
@@ -6,53 +6,52 @@ Currently has support for creating a new contact and new message
 
 package nutshell
 
-import(
-	"log"
-	"encoding/json"
-	"net/http"
+import (
 	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
-
-type NutshellConfig struct{
-	User string `jason:"user"`
-	ApiKey string  `json:"apiKey"`
+type NutshellConfig struct {
+	User   string `jason:"user"`
+	ApiKey string `json:"apiKey"`
 }
 
 var config NutshellConfig
 
-func Init(nutConfig NutshellConfig){
+func Init(nutConfig NutshellConfig) {
 	config = nutConfig
-}  
-
-type APIS struct{
-	Id int 			`json:"id"`
-	Method string 		`json:"method"`
-	Params interface{} 	`json:"params"`
 }
 
-type ContactCallS struct {
-	Contact ContactS `json:"contact,omitempty"`
+type APICall struct {
+	Id     int         `json:"id"`
+	Method string      `json:"method"`
+	Params interface{} `json:"params"`
 }
 
-type ContactS struct {
-	Name        string       `json:"name,omitempty"`
-	//Owner       OwnerS       `json:"owner,omitempty"`
-	Description string       `json:"description,omitempty"`
-	Phone       []string     `json:"phone,omitempty"`
-	Email       []string     `json:"email,omitempty"`
-	Address     []AddressS   `json:"address,omitempty"`
-	Lead        []LeadS      `json:"lead,omitempty"`
+type ContactCall struct {
+	Contact Contact `json:"contact,omitempty"`
+}
+
+type Contact struct {
+	Name string `json:"name,omitempty"`
+	//Owner       Owner       `json:"owner,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Phone       []string   `json:"phone,omitempty"`
+	Email       []string   `json:"email,omitempty"`
+	Address     []Address `json:"address,omitempty"`
+	Lead        []Lead    `json:"lead,omitempty"`
 	//CustomField CustomFieldS `json:"customField,omitempty"`
-	TerritoryId int          `json:"territoryId,omitempty"`
+	TerritoryId int `json:"territoryId,omitempty"`
 }
 
-type OwnerS struct {
+type Owner struct {
 	EntityType string `json:"enityType,omitempty"`
 	Id         int    `json:"id,omitempty"`
 }
 
-type AddressS struct {
+type Address struct {
 	Address_1  string `json:"address_1,omitempty"`
 	Address_2  string `json:"address_2,omitempty"`
 	Address_3  string `json:"address_3,omitempty"`
@@ -62,60 +61,60 @@ type AddressS struct {
 	Country    string `json:"country,omitempty"`
 }
 
-type LeadS struct {
+type Lead struct {
 	Relationship string `json:"relationship,omitempty"`
 	Id           int    `json:"id,omitempty"`
 }
 
-type CustomFieldS struct {
+type CustomField struct {
 	Id int `json:"id,omitempty"`
 }
 
-type ResultS struct{
-	Result ContactResultS `json:"result,omitempty"`
-	Error ErrorS `json:"error"`
+type Result struct {
+	Result ContactResult `json:"result,omitempty"`
+	Error  Error         `json:"error"`
 	//did not implement everthing
 }
 
-type ErrorS struct{
-	Code int `json:"code"`
+type Error struct {
+	Code    int    `json:"code"`
 	Message string `json:"message"`
-	Data string `json:"data"`
+	Data    string `json:"data"`
 }
-type ContactResultS struct{
+type ContactResult struct {
 	Id int `json:"id,omitempty"`
 }
 
-type NoteCallS struct{
-	Entity EntityS `json:"entity"`
-	Note string `json:"note"`
+type NoteCall struct {
+	Entity Entity `json:"entity"`
+	Note   string  `json:"note"`
 }
 
-type EntityS struct{
+type Entity struct {
 	EntityType string `json:"entityType"`
-	Id int `json:"id"`
+	Id         int    `json:"id"`
 }
 
 //APICall takes in APIS and marshals it into a JSON sent by HTTP to nutshell
-func ApiCall(call APIS, response interface{}) {
+func ApiCall(call APICall, response interface{}) {
 
 	//Marshal APIS into a JSON for HTTP Body
 	var buf bytes.Buffer
 	d := json.NewEncoder(&buf)
 	err := d.Encode(call)
-	if(err != nil){
+	if err != nil {
 		log.Printf("ERROR with json marshaling: %s\n", err)
 	}
-	log.Printf("%s\n\n\n",buf)
+	log.Printf("%s\n", buf)
 
 	req, err := http.NewRequest("POST", "https://app01.nutshell.com/api/v1/json", bytes.NewReader(buf.Bytes()))
-	if(err != nil){
+	if err != nil {
 		log.Printf("Error creating HTTP request: %s\n", err)
 		return
 	}
 
 	req.SetBasicAuth(config.User, config.ApiKey)
-		
+
 	client := &http.Client{}
 
 	resp, err := client.Do(req)
@@ -123,11 +122,11 @@ func ApiCall(call APIS, response interface{}) {
 		log.Printf("HTTP Responce Error: %s\n", err)
 		return
 	}
-	
+
 	defer resp.Body.Close()
 
 	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(responce)
+	err = dec.Decode(response)
 	if err != nil {
 		log.Printf("ERROR with reading HTTP response body: %s\n", err)
 	}
